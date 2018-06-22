dd61479bcf7440b4148c00ea62c733948fec804b
aswathy <aswathyseb@gmail.com>
2018-06-22 11:28:34 -0400

    Update by asebastian on Fri Jun 22 11:28:34 EDT 2018 from Aswathys-iMac.local

--- recipes/classify/classify-qiime.sh
+++ recipes/classify/classify-qiime.sh
@@ -2,7 +2,13 @@ set -ue
 
 export JAVA_LD_LIBRARY_PATH=""
 
+# Include the path to qiime2 environment here.
 source activate /Users/asebastian/miniconda3/envs/qiime2
+#source activate /home/www/miniconda3/envs/qiime2
+
+# This is needed only in Linux machine.
+#export LC_ALL=C.UTF-8
+#export LANG=C.UTF-8
 
 # The input directory for the data
 DDIR=$(dirname {{reads.value}})
@@ -45,6 +51,10 @@ N=4
 STORE=results
 mkdir -p $STORE
 
+# Directory that holds input data as qiime2 artifact.
+DATA=$STORE/data
+mkdir -p $DATA
+
 # Directory that holds DADA2 results
 DADA2=$STORE/dada2
 
@@ -54,32 +64,32 @@ BLAST=$STORE/blast-taxa
 # Import the input files as a qiime2 artifact.
 echo "Importing data as qiime2 artifact."
 
-{% if LIBRARY == "SE" %}
+{% if library.value == "SE" %}
 
 qiime tools import --type 'SampleData[SequencesWithQuality]'  --input-path $DDIR \
---source-format CasavaOneEightSingleLanePerSampleDirFmt --output-path $STORE/data.qza
+--source-format CasavaOneEightSingleLanePerSampleDirFmt --output-path $DATA/data.qza
 
 {% else %}
 
 qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]'  --input-path $DDIR \
---source-format CasavaOneEightSingleLanePerSampleDirFmt --output-path $STORE/data.qza
+--source-format CasavaOneEightSingleLanePerSampleDirFmt --output-path $DATA/data.qza
 
 {% endif %}
 
 echo "Generating summary data."
-qiime demux summarize --i-data $STORE/data.qza --o-visualization $STORE/data.qzv
+qiime demux summarize --i-data $DATA/data.qza --o-visualization $DATA/data.qzv
 
 # De-noising with Dada2 and feature table generation.
 echo "De-noising with dada2."
 
-{% if LIBRARY == "SE" %}
+{% if library.value == "SE" %}
 
-qiime dada2 denoise-single --verbose  --i-demultiplexed-seqs $STORE/data.qza --output-dir $DADA2 --p-n-threads $N \
---p-trunc-len $POS_RIGHT_F --p-trim-left $POS_LEFT_F $POS_LEFT_R
+qiime dada2 denoise-single --verbose  --i-demultiplexed-seqs $DATA/data.qza --output-dir $DADA2 --p-n-threads $N \
+--p-trunc-len $POS_RIGHT_F --p-trim-left $POS_LEFT_F
 
 {% else %}
 
-qiime dada2 denoise-paired --verbose  --i-demultiplexed-seqs $STORE/data.qza --output-dir $DADA2 --p-n-threads $N \
+qiime dada2 denoise-paired --verbose  --i-demultiplexed-seqs $DATA/data.qza --output-dir $DADA2 --p-n-threads $N \
 --p-trunc-len-f $POS_RIGHT_F --p-trunc-len-r $POS_RIGHT_R --p-trim-left-f $POS_LEFT_F --p-trim-left-r $POS_LEFT_R
 
 {% endif %}
